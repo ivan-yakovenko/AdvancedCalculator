35e3d98725cf9098b1f6c38a3aff5b698e9fa4b5
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-07-17 13:49:16 +0300

    add3d declaration for function support

--- Assignment_5/RPN/RPN.h
+++ Assignment_5/RPN/RPN.h
@@ -8,6 +8,8 @@ public:
     string infixToPostfix(string expression);
     bool isOperator(char token);
     bool isOperand(char token);
+    bool isFunction(string token);
+    bool isLetter(char token);
     int getPriority(char token);
     double rpn(string tokens);
 };

4ea036fc6042602ca8a53405051e97c1e93da0e1
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-07-17 13:49:50 +0300

    added implementation for function support

--- Assignment_5/RPN/RPN.cpp
+++ Assignment_5/RPN/RPN.cpp
@@ -1,11 +1,20 @@
 #include "RPN.h"
+#include <sstream>
 
 bool RPN::isOperator(char token) {
     return token == '+' || token == '-' || token == '*' || token == '/';
 }
 
 bool RPN::isOperand(char token) {
-    return !isOperator(token) && token != '('&& token != ')' && token != ' ';
+    return isdigit(token) || token == '.';
+}
+
+bool RPN::isFunction(string token) {
+    return token == "max" || token == "min" || token == "abs" || token == "pow";
+}
+
+bool RPN::isLetter(char token) {
+    return isalpha(token);
 }
 
 int RPN::getPriority(char token) {
@@ -27,30 +36,52 @@ string RPN::infixToPostfix(string expression) {
     string postfix;
     Stack<char> operators;
     string currentNumber;
+    Stack<string> functions;
+    string currentFunction;
 
     for (char element : expression) {
         if(isOperand(element)) {
             currentNumber += element;
+        }
+        else if(isLetter(element)) {
+            currentFunction += element;
         }
         else {
             if(!currentNumber.empty()) {
                 postfix += currentNumber + ' ';
                 currentNumber = "";
             }
+            if(!currentFunction.empty()) {
+                functions.push(currentFunction);
+                currentFunction = "";
+            }
             if(element == '(') {
                 operators.push(element);
             }
-            else if(element == ')') {
-                while(!operators.isEmpty() && operators.getTop() != '(') {
+            else if(element == ',') {
+                while (!operators.isEmpty() && operators.getTop() != '(') {
                     postfix += operators.getTop();
                     postfix += ' ';
                     operators.pop();
                 }
+            }
+            else if(element == ')') {
+                while (!operators.isEmpty() && operators.getTop() != '(') {
+                    postfix += operators.getTop();
+                    postfix += ' ';
+                    operators.pop();
+                }
+
                 if (!operators.isEmpty()) {
                     operators.pop();
                 }
-            }
 
+                if (!functions.isEmpty()) {
+                    postfix += functions.getTop();
+                    postfix += ' ';
+                    functions.pop();
+                }
+            }
             else if(isOperator(element)) {
                 while(!operators.isEmpty() && getPriority(operators.getTop()) >= getPriority(element) && operators.getTop() != '(') {
                     postfix += operators.getTop();
@@ -78,28 +109,39 @@ string RPN::infixToPostfix(string expression) {
 double RPN::rpn(string tokens) {
     Stack<double> rpntokens;
     string currentNumber;
+    string currentFunction;
+    istringstream tokenStream(tokens);
+    string token;
 
-    for(char token : tokens) {
-        if(isOperand(token)) {
-            currentNumber += token;
-        }
-        else if(isspace(token)) {
-            if(!currentNumber.empty()) {
-                rpntokens.push(stod(currentNumber));
-                currentNumber = "";
+    while(tokenStream >> token) {
+        if(isFunction(token)) {
+            double operand1 = rpntokens.getTop();
+            rpntokens.pop();
+            if(token == "abs") {
+                rpntokens.push(abs(operand1));
+            }
+            else {
+                double operand2 = rpntokens.getTop();
+                rpntokens.pop();
+                if(token == "max" ) {
+                    rpntokens.push(max(operand2, operand1));
+                }
+                else if(token == "min" ) {
+                    rpntokens.push(min(operand2, operand1));
+                }
+                else if(token == "pow" ) {
+                    rpntokens.push(pow(operand2, operand1));
+                }
             }
         }
-        else if(isOperator(token)) {
-            if(!currentNumber.empty()) {
-                rpntokens.push(stod(currentNumber));
-                currentNumber = "";
-            }
+        else if(isOperator(token[0]) && token.size() == 1) {
+            char operation = token[0];
             double operand1 = rpntokens.getTop();
             rpntokens.pop();
             double operand2 = rpntokens.getTop();
             rpntokens.pop();
 
-            switch (token) {
+            switch (operation) {
                 case '+': {
                     rpntokens.push(operand2 + operand1);
                     break;
@@ -121,6 +163,8 @@ double RPN::rpn(string tokens) {
                     break;
                 }
             }
+        } else {
+            rpntokens.push(stod(token));
         }
     }
     return rpntokens.getTop();
